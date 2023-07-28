4ae217eda7a43bfa415d36c42b9d1f0e0beb3d98
gabldotink <99302669+gabldotink@users.noreply.github.com>
2023-07-28 03:38:28 +0000

    uncapitalize you in copyright notice

--- sharkive.sh
+++ sharkive.sh
@@ -8,9 +8,11 @@
 
 # limit lines to 80 printed characters when possible --------------------------
 
-# This script is free software, dedicated to the public domain. You, the
+# This script is free software, dedicated to the public domain. you, the
 # individual or other entity receiving the software, may use, redistribute, and
-# modify it as You wish without asking permission or crediting the author(s).
+# modify it as you wish without asking permission or crediting the author(s).
 # These rights that are granted to You may be affirmed by either
 # Creative Commons Zero 1.0 Universal (CC0 1.0) [./COPYING.CC0-1.0]
 # or the Zero-Clause BSD License [./COPYING.0BSD], at your own preference.
+
+
