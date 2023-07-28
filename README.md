f289a4317f90ecc8e77818fe99d4275bcd41a14e
gabldotink <99302669+gabldotink@users.noreply.github.com>
2023-07-28 03:33:16 +0000

    update copyright info

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ sharkive [https://github.com/gabldotink/sharkive]
 SPDX-License-Identifier: CC0-1.0 OR 0BSD
 -->
 
+<!-- limit lines to 80 printed characters when possible ------------------- -->
+
 # `sharkive`
 
 `sharkive`: a POSIX shell script to make it easy to contribute to the
--- sharkive.sh
+++ sharkive.sh
@@ -5,3 +5,12 @@
 # sharkive [https://github.com/gabldotink/sharkive]
 # /sharkive.sh
 # SPDX-License-Identifier: CC0-1.0 OR 0BSD
+
+# limit lines to 80 printed characters when possible --------------------------
+
+# This script is free software, dedicated to the public domain. You, the
+# individual or other entity receiving the software, may use, redistribute, and
+# modify it as You wish without asking permission or crediting the author(s).
+# These rights that are granted to You may be affirmed by either
+# Creative Commons Zero 1.0 Universal (CC0 1.0) [./COPYING.CC0-1.0]
+# or the Zero-Clause BSD License [./COPYING.0BSD], at your own preference.
