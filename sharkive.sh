391e3e78cae2e3ac5e95db60766c6ada2c9ca581
gabldotink <99302669+gabldotink@users.noreply.github.com>
2023-07-28 04:07:00 +0000

    disclaim warranties in readme and script
    “this variable makes bash all POSIX-y” is one of the worst
    phrases I’ve ever written

--- sharkive.sh
+++ sharkive.sh
@@ -8,9 +8,16 @@
 
 # limit lines to 80 printed characters when possible --------------------------
 
-# This script is free software, dedicated to the public domain. you, the
+# Copyright 2023 gabldotink <gabl@gabl.ink> and other sharkive contributors.
+# This script is free software, dedicated to the public domain. You, the
 # individual or other entity receiving the software, may use, redistribute, and
 # modify it as you wish without asking permission or crediting the author(s).
 # These rights that are granted to You may be affirmed by either
 # Creative Commons Zero 1.0 Universal (CC0 1.0) [./COPYING.CC0-1.0]
 # or the Zero-Clause BSD License [./COPYING.0BSD], at your own preference.
+# The software is provided as-is, with no warranties of any kind to the extent
+# permissible by relevant laws. See the license texts for more details.
+
+# this variable makes bash all POSIX-y
+readonly POSIXLY_CORRECT='true'
+export POSIXLY_CORRECT
